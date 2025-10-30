951e1fd3605b629a7cd9a5fbf4674cd51d9938d8
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-30 14:01:21 +0900

    Mengimplementasi Radio Button untuk pilihan jenis kelamin

--- app/src/main/java/com/example/userlayout/TeksEdit.kt
+++ app/src/main/java/com/example/userlayout/TeksEdit.kt
@@ -2,10 +2,13 @@ package com.example.userlayout
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -46,7 +49,17 @@ fun FormDataDiri(modifier: Modifier
         )
         Row {
             gender.forEach { item ->
-
+                Row (modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
+                }
             }
         }
     }
