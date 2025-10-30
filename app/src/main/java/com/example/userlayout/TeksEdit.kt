4771cb695eac496826aca6a6abdad16c80c430dc
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-30 14:24:02 +0900

    Menambahkan divider lagi sebagai pemisah komponen di form

--- app/src/main/java/com/example/userlayout/TeksEdit.kt
+++ app/src/main/java/com/example/userlayout/TeksEdit.kt
@@ -92,7 +92,14 @@ fun FormDataDiri(modifier: Modifier
                 alamat = textAlamat
             }
         ) {
-            Text( stringResource("Submit"))
+            Text( stringResource())
         }
+
+        Divider( modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+            id = R.dimen.padding_medium
+        )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
     }
 }
